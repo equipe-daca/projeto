b7ddb98801e3ff2ef0cae8fb45707bc8ef39f18e
Gervásio Júnior <juniorb08@gmail.com>
2016-07-24 01:11:58 -0300

    Adiciona código inicial e .gitignore

---
+++ src/main/java/br/edu/ufcg/SampleController.java
@@ -0,0 +1,15 @@
+package br.edu.ufcg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World!";
+    }
+}
