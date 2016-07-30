22b2b18f7233608274f3dd7da6daa55b08635be0
Diego Augusto <daugustomoura@gmail.com>
2016-07-30 13:56:57 -0300

    Edita classe Solutuion, Adiciona testes para rota de Solution

--- src/test/java/br/edu/ufcg/SolutionTest.java
+++ src/test/java/br/edu/ufcg/SolutionTest.java
@@ -1,11 +1,19 @@
 package br.edu.ufcg;
 
+import br.edu.ufcg.models.Solution;
+import com.google.gson.Gson;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @SpringApplicationConfiguration(classes=Application.class)
 @WebIntegrationTest("server.port=0")
@@ -17,6 +25,20 @@ public class SolutionTest {
 
     @Test
     public void createSolution() throws Exception {
+        Gson gson = new Gson();
 
+        Solution solution = new Solution("Body", new HashMap<>());
+
+        String json = gson.toJson(solution);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(json)
+                .when()
+                .port(this.port)
+                .post("/solution")
+                .then()
+                .assertThat()
+                .statusCode(is(200));
     }
 }
